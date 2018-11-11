a94edbc09a5cab8b46693d7e1f22715fe2ce6ed8
Ben <bchadwick94@gmail.com>
2018-11-11 15:34:43 +0000

    adding read me

---
+++ README.md
@@ -0,0 +1,7 @@
+## Infrastructure setup for a heroku pipeline
+
+- One pipeline
+- Two apps (staging & production)
+- Two postgresql DBs
+
+*REPO does not include a variables file which should be stored locally and included in the .gitignore. Two variables used in this setup are app_name and heroku_api_key*
--- pipeline.tf
+++ pipeline.tf
@@ -6,28 +6,28 @@ provider "heroku" {
 
 # Create Heroku apps for staging and production
 resource "heroku_app" "staging" {
-  name = "gas-invest-staging"
+  name = "${var.app_name}-staging"
 }
 
 resource "heroku_app" "production" {
-  name = "gas-invest-production"
+  name = "${var.app_name}-production"
 }
 
 # Create a Heroku pipeline
-resource "heroku_pipeline" "gas-invest" {
-  name = "gas-invest"
+resource "heroku_pipeline" "app_pipeline" {
+  name = "${var.app_name}"
 }
 
 # Couple apps to different pipeline stages
 resource "heroku_pipeline_coupling" "staging" {
   app      = "${heroku_app.staging.name}"
-  pipeline = "${heroku_pipeline.gas-invest.id}"
+  pipeline = "${heroku_pipeline.app_pipeline.id}"
   stage    = "staging"
 }
 
 resource "heroku_pipeline_coupling" "production" {
   app      = "${heroku_app.production.name}"
-  pipeline = "${heroku_pipeline.gas-invest.id}"
+  pipeline = "${heroku_pipeline.app_pipeline.id}"
   stage    = "production"
 }
 
